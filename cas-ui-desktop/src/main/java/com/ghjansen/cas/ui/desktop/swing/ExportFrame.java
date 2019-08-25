81398547a7ca321352be9f4e764a7cdf257ebfc3
Guilherme Humberto Jansen <contact.ghjansen@gmail.com>
2019-08-24 22:20:55 -0300

    Visual components for advanced export options. Issue #52.

--- cas-ui-desktop/src/main/java/com/ghjansen/cas/ui/desktop/swing/ExportFrame.java
+++ cas-ui-desktop/src/main/java/com/ghjansen/cas/ui/desktop/swing/ExportFrame.java
@@ -28,39 +28,156 @@ import java.awt.Font;
 
 import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  * @author Guilherme Humberto Jansen (contact.ghjansen@gmail.com)
  */
 public class ExportFrame extends JFrame {
+	private JTextField txtCellScale;
+	private JTextField txtGridLinesThickness;
+	private JTextField txtCellLinesColour;
 	public ExportFrame() {
 		super();
+		setAlwaysOnTop(true);
 		setTitle("Advanced Export Options (ALPHA)");
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 425, 329);
+		setBounds(100, 100, 300, 248);
+		getContentPane().setLayout(null);
 		JPanel panelExportAO = new JPanel();
+		panelExportAO.setBounds(6, 6, 288, 177);
 		panelExportAO.setBorder(new TitledBorder(null, "Advanced Export Options", TitledBorder.LEFT, TitledBorder.TOP, new Font("Lucida Grande", Font.BOLD, 12), Color.BLACK));
-		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(panelExportAO, GroupLayout.DEFAULT_SIZE, 438, Short.MAX_VALUE)
-					.addContainerGap())
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(panelExportAO, GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
-					.addContainerGap())
-		);
 		
-		JLabel lblWarningAO = new JLabel("WARNING: This feature is a prototype and still under development.");
-		lblWarningAO.setForeground(Color.RED);
-		lblWarningAO.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
-		panelExportAO.add(lblWarningAO);
-		getContentPane().setLayout(groupLayout);
+		getContentPane().add(panelExportAO);
+		
+		JLabel lblWarning1AO = new JLabel("WARNING: This is a development prototype!");
+		lblWarning1AO.setBackground(Color.BLACK);
+		lblWarning1AO.setForeground(Color.RED);
+		lblWarning1AO.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		
+		JLabel lblCellScale = new JLabel("Cell Scale:");
+		lblCellScale.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		
+		JLabel lblShowGridLines = new JLabel("Show Grid Lines:");
+		lblShowGridLines.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		
+		JLabel lblGridLinesThickness = new JLabel("Grid Lines Thickness:");
+		lblGridLinesThickness.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		
+		JLabel lblGridLinesColour = new JLabel("Grid Lines Colour:");
+		lblGridLinesColour.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		
+		txtCellScale = new JTextField();
+		txtCellScale.setText("1");
+		txtCellScale.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		txtCellScale.setColumns(10);
+		
+		JRadioButton rdbtnYes = new JRadioButton("Yes");
+		rdbtnYes.setEnabled(false);
+		rdbtnYes.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		
+		JRadioButton rdbtnNo = new JRadioButton("No");
+		rdbtnNo.setEnabled(false);
+		rdbtnNo.setSelected(true);
+		rdbtnNo.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		
+		txtGridLinesThickness = new JTextField();
+		txtGridLinesThickness.setEnabled(false);
+		txtGridLinesThickness.setText("1");
+		txtGridLinesThickness.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		txtGridLinesThickness.setColumns(10);
+		
+		txtCellLinesColour = new JTextField();
+		txtCellLinesColour.setEnabled(false);
+		txtCellLinesColour.setText("#888888");
+		txtCellLinesColour.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		txtCellLinesColour.setColumns(10);
+		
+		GroupLayout gl_panelExportAO = new GroupLayout(panelExportAO);
+		gl_panelExportAO.setHorizontalGroup(
+			gl_panelExportAO.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelExportAO.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panelExportAO.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblWarning1AO, GroupLayout.DEFAULT_SIZE, 264, Short.MAX_VALUE)
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addComponent(lblCellScale, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(txtCellScale, GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE))
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addComponent(lblShowGridLines, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(rdbtnYes)
+							.addGap(12)
+							.addComponent(rdbtnNo))
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addComponent(lblGridLinesThickness)
+							.addGap(12)
+							.addComponent(txtGridLinesThickness, GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE))
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addComponent(lblGridLinesColour, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(txtCellLinesColour, GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
+		gl_panelExportAO.setVerticalGroup(
+			gl_panelExportAO.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelExportAO.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblWarning1AO, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE)
+					.addGroup(gl_panelExportAO.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addGap(11)
+							.addComponent(lblCellScale, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtCellScale, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panelExportAO.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addGap(4)
+							.addComponent(lblShowGridLines, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE))
+						.addComponent(rdbtnYes)
+						.addComponent(rdbtnNo))
+					.addGroup(gl_panelExportAO.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addGap(9)
+							.addComponent(lblGridLinesThickness, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addGap(4)
+							.addComponent(txtGridLinesThickness, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)))
+					.addGroup(gl_panelExportAO.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addGap(11)
+							.addComponent(lblGridLinesColour, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelExportAO.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtCellLinesColour, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)))
+					.addGap(76))
+		);
+		panelExportAO.setLayout(gl_panelExportAO);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				txtCellScale.requestFocus();
+			}
+		});
+		btnCancel.setBounds(6, 189, 138, 29);
+		btnCancel.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		getContentPane().add(btnCancel);
+		
+		JButton btnExport = new JButton("Export");
+		btnExport.setBounds(156, 189, 138, 29);
+		btnExport.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		getContentPane().add(btnExport);
 		this.setResizable(false);
 		this.setVisible(false);
 	}
