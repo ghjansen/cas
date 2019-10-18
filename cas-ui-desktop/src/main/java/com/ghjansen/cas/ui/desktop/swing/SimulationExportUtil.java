82e0c87e1b15b75f4480c55ccb563eb95555ea5c
Guilherme Humberto Jansen <contact.ghjansen@gmail.com>
2019-10-18 17:45:01 -0300

    Support for grid line thickness while exporting simulation. Issue #59.

--- cas-ui-desktop/src/main/java/com/ghjansen/cas/ui/desktop/swing/SimulationExportUtil.java
+++ cas-ui-desktop/src/main/java/com/ghjansen/cas/ui/desktop/swing/SimulationExportUtil.java
@@ -34,29 +34,35 @@ public class SimulationExportUtil {
 	public static BufferedImage createBufferedImage(UnidimensionalSimulationController simulationController, 
 			int cellScale, boolean showGrid, int gridLineThickness, Color gridLineColour){
 		
-		if(cellScale < 1) cellScale = 1; //replace invalid by default
+		//replace invalid values by default values, for cellScale and gridLineThickness
+		if(cellScale < 1) cellScale = 1;
+		boolean bellowLowerLimit = !(gridLineThickness >= 1);
+		boolean overHigherLimit = !(gridLineThickness <= (cellScale-1)/2);
+		if(bellowLowerLimit || overHigherLimit) gridLineThickness = 1;
 		
+		//calculate the size of the simulation and image
 		int simulationWidth = simulationController.getSimulation().getUniverse().getTime().getRelative().get(0).getLimit();
 		int simulationHeight = simulationController.getSimulation().getUniverse().getTime().getLimit() + 1;
 		int bufferWidth = simulationWidth * cellScale;
 		int bufferHeight = simulationHeight * cellScale;
 		
+		//create buffered image and graphics
 		BufferedImage buffer = new BufferedImage(bufferWidth, bufferHeight, BufferedImage.TYPE_INT_RGB);
 		Graphics2D g = buffer.createGraphics();
 		
-		//cells
+		//draw cells
 		drawCells(g, simulationController, cellScale, bufferHeight);
 		
-		//grid lines
+		//draw grid lines
 		if(showGrid && cellScale >= 3){
-			drawGrid(g, cellScale, bufferWidth, bufferHeight);
+			drawGrid(g, cellScale, bufferWidth, bufferHeight, gridLineThickness);
 		}
 		
 		return buffer;
 	}
 	
 	private static void drawCells(Graphics2D g, UnidimensionalSimulationController simulationController, int cellScale, int bufferHeight){
-		// initial condition
+		// initial condition cells
 		for(int i = 0; i < simulationController.getSimulation().getUniverse().getSpace().getInitial().size(); i++){
 			UnidimensionalCell c = (UnidimensionalCell) simulationController.getSimulation().getUniverse().getSpace().getInitial().get(i);
 			Color color = c.getState().getValue() == 0 ? Color.white : Color.black;
@@ -65,7 +71,7 @@ public class SimulationExportUtil {
 			//buffer.setRGB(i, 0, color.getRGB());
 			
 		}
-		// history
+		// history cells
 		for(int j = 0; j < simulationController.getSimulation().getUniverse().getSpace().getHistory().size(); j++){
 			List<UnidimensionalCell> cells = simulationController.getSimulation().getUniverse().getSpace().getHistory().get(j);
 			int y = (j+1) * cellScale;
@@ -77,7 +83,7 @@ public class SimulationExportUtil {
 				//buffer.setRGB(i, j+1, color.getRGB());
 			}
 		}
-		// current/last
+		// current/last cells
 		for(int i = 0; i < simulationController.getSimulation().getUniverse().getSpace().getCurrent().size(); i++){
 			UnidimensionalCell c = (UnidimensionalCell) simulationController.getSimulation().getUniverse().getSpace().getCurrent().get(i);
 			Color color = c.getState().getValue() == 0 ? Color.white : Color.black;
@@ -87,25 +93,25 @@ public class SimulationExportUtil {
 		}
 	}
 	
-	private static void drawGrid(Graphics2D g, int cellScale, int bufferWidth, int bufferHeight){
+	private static void drawGrid(Graphics2D g, int cellScale, int bufferWidth, int bufferHeight, int gridLineThickness){
 		int px = 0, py = cellScale;
 		g.setColor(Color.gray);
 		//horizontal lines (bottom)
 		for(int i = 1; py <= bufferHeight;){
-			g.fillRect(0, py-1, bufferWidth, 1);
+			g.fillRect(0, py-gridLineThickness, bufferWidth, gridLineThickness);
 			i++;
 			py = cellScale * i;
 		}
 		//vertical lines (left)
 		for(int i = 0; px < bufferWidth;){
-			g.fillRect(px, 0, 1, bufferHeight);
+			g.fillRect(px, 0, gridLineThickness, bufferHeight);
 			i++;
 			px = cellScale * i;
 		}
 		//horizontal line (top)
-		g.fillRect(0, 0, bufferWidth, 1);
+		g.fillRect(0, 0, bufferWidth, gridLineThickness);
 		//vertical line (right)
-		g.fillRect(bufferWidth-1, 0, 1, bufferHeight);
+		g.fillRect(bufferWidth-gridLineThickness, 0, gridLineThickness, bufferHeight);
 	}
 	
 }
